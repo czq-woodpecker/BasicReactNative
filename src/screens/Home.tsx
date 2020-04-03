7c909dcfbb4ffc73af083bf26915d1cfb02e45d7
zeqiang.chen <1209120868@qq.com>
2020-04-04 01:10:42 +0800

    [zeqiang] feature: adjust style

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -16,12 +16,26 @@ export class Home extends React.Component<Props> {
 
   render() {
     return (
-      <View>
-        <Button
-          title={'User List'}
-          onPress={() => this.props.navigation.navigate('UserList')}
-        />
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title={'User List'}
+            onPress={() => this.props.navigation.navigate('UserList')}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    width: 100,
+  },
+});
